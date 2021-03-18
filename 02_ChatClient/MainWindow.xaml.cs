c77e2ae18902161227ce87bdd6b8298d030869a9
Микола Селецький <Seletskyi.Mykola@outlook.com>
2021-03-18 23:19:27 +0200

    Add project files.

---
+++ 02_ChatClient/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02_ChatClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // адреса віддаленого хоста
+        private static string remoteIPAddress = "127.0.0.1";
+        // порт віддаленого хоста
+        private static int remotePort = 8080;
+        // створення об'єкту UdpClient для відправки даних
+        UdpClient client = new UdpClient(0);
+
+        ObservableCollection<MessageInfo> messages = new ObservableCollection<MessageInfo>();
+        string MyName = "Andriy";
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            bool UniqueNickname = false;
+            while (!UniqueNickname)
+            {
+                Nickname nickname = new Nickname();
+                if (nickname.ShowDialog() == true)
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show("Is not a unique nickname");
+                }
+            }
+            list.ItemsSource = messages;
+            Task.Run(() => Listen());
+        }
+
+        private void Listen()
+        {
+            IPEndPoint iPEndPoint = null;
+            while (true)
+            {
+                try
+                {
+                    byte[] data = client.Receive(ref iPEndPoint);
+
+                    string msg = Encoding.UTF8.GetString(data);
+
+                    _ = Dispatcher.BeginInvoke(new Action(() =>
+                      {
+                          MessageInfo.TypeMessage typeMessage;
+                          string text = "";
+                          string user = "";
+                          if (msg[0] == '@')
+                          {
+                              text = msg.Substring(1);
+                              typeMessage = MessageInfo.TypeMessage.InfoMessage;
+                          }
+                          else
+                          {
+                              user = msg.Substring(0, msg.IndexOf('#'));
+                              if (MyName == user)
+                              {
+                                  typeMessage = MessageInfo.TypeMessage.MyMessage;
+                              }
+                              else
+                              {
+                                  typeMessage = MessageInfo.TypeMessage.TextMessage;
+                              }
+                              text = msg.Substring(msg.IndexOf('#')+1);
+                          }
+                          messages.Insert(0, new MessageInfo()
+                          {
+                              User = user,
+                              Time = DateTime.Now.ToShortTimeString(),
+                              Text = text,
+                              @Type = typeMessage
+                          });
+                      }));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void SendMessage(string msg)
+        {
+            if (string.IsNullOrWhiteSpace(msg)) return;
+
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(remoteIPAddress), remotePort);
+
+            byte[] data = Encoding.UTF8.GetBytes(msg);
+            client.Send(data, data.Length, iPEndPoint);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage("#2");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SendMessage($"#1{MyName}");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            SendMessage(txtBox.Text);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            SendMessage("#2");
+            client.Close();
+        }
+    }
+}
---
+++ 02_Chat_Server/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Chat_Server
+{
+    class Program
+    {
+        // порт для прослуховування
+        private const int port = 8080;
+        // список учасників чату
+        //private static List<IPEndPoint> members = new List<IPEndPoint>();
+        private static Dictionary<IPEndPoint, string> members = new Dictionary<IPEndPoint, string>();
+        static void Main(string[] args)
+        {
+            // створення об'єкту UdpClient та встановлюємо порт для прослуховування
+            UdpClient server = new UdpClient(port);
+            // створюємо об'єкт для збреження адреси віддаленого хоста
+            IPEndPoint groupEP = null;
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("\tWaiting for a message...");
+                    byte[] bytes = server.Receive(ref groupEP);
+
+                    // конвертуємо масив байтів в рядок
+                    string msg = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+
+                    bool isSuccesful;
+                    if (msg.Substring(0,2) == "#1")
+                    {
+                        isSuccesful = AddMember(groupEP, msg.Substring(2));
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Request to connect from {groupEP} at {DateTime.Now.ToShortTimeString()}\n");
+                        Console.WriteLine($"Name : {msg.Substring(2)}");
+                        if (isSuccesful)
+                        {
+                            Console.WriteLine($"Operation completed succesful!\n");
+
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            string str = $"@{msg.Substring(2)} connected";
+                            byte[] strByteArr= Encoding.UTF8.GetBytes(str);
+                            foreach (var m in members)
+                            {
+                                try
+                                {
+                                    server.Send(strByteArr, strByteArr.Length, m.Key);
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine($"Error with {m}: {ex.Message}\n");
+                                }
+                            }
+                        }
+                    }
+                    else if (msg.Substring(0, 2) == "#2")
+                    {
+                        string name = members[groupEP];
+                        isSuccesful = RemoveMember(groupEP);
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"Request to leave from {groupEP} at {DateTime.Now.ToShortTimeString()}\n");
+                        if (isSuccesful)
+                        {
+                            Console.WriteLine($"Operation completed succesful!\n");
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            string str = $"@{name} disconnected";
+                            byte[] strByteArr = Encoding.UTF8.GetBytes(str);
+                            foreach (var m in members)
+                            {
+                                try
+                                {
+                                    server.Send(strByteArr, strByteArr.Length, m.Key);
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine($"Error with {m}: {ex.Message}\n");
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine($"Message from {groupEP} at {DateTime.Now.ToShortTimeString()}: {msg}\n");
+                        string str = $"{members[groupEP]}#{msg.Substring(2)}";
+                        byte[] strByteArr = Encoding.UTF8.GetBytes(str.ToString());
+                        foreach (var m in members)
+                        {
+                            try
+                            {
+                                server.Send(strByteArr, strByteArr.Length, m.Key);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine($"Error with {m}: {ex.Message}\n");
+                            }
+                        }
+                    }
+                    Console.ResetColor();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                // закриття з'єднання
+                server.Close();
+            }
+        }
+
+        static bool AddMember(IPEndPoint endPoint,string name)
+        {
+            var member = members.FirstOrDefault(m => m.ToString() == endPoint.ToString());
+            if (member.Value == null)
+            {
+                if (members.Values.Where(i=>i==name).Count()==0)
+                {
+                    members.Add(endPoint, name);
+                    return true;
+                }
+            }
+            return false;
+        }
+        static bool RemoveMember(IPEndPoint endPoint)
+        {
+            return members.Remove(endPoint);
+        }
+    }
+}
